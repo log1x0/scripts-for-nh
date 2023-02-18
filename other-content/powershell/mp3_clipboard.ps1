f8fa537eb8314ccbb67fc4b5dc0137ecfec97e90
log1x0 <107345625+log1x0@users.noreply.github.com>
2023-02-18 09:56:06 +0100

    Update mp3_clipboard.ps1 (#34)

--- other-content/powershell/mp3_clipboard.ps1
+++ other-content/powershell/mp3_clipboard.ps1
@@ -6,7 +6,16 @@
 # and make sure to have created a *path system environment variable* correctly before
 #
 Add-Type -Assembly PresentationCore
-$CLIPBOARD_TEXT = [Windows.Clipboard]::GetText()
-Write-Output $CLIPBOARD_TEXT
-yt-dlp.exe -f bestaudio -x --audio-format mp3 "$CLIPBOARD_TEXT"
+$i = 1000
+while ($i -gt 0) {
+    Write-Output $i
+    $CLIPBOARD_TEXT = [Windows.Clipboard]::GetText()
+    if ($CLIPBOARD_TEXT -match 'https://www\.youtube\.com/watch\?v=.{6,}') {
+        Write-Output $CLIPBOARD_TEXT
+        yt-dlp.exe -f bestaudio -x --audio-format mp3 "$CLIPBOARD_TEXT"
+        Set-Clipboard -Value "This is a test string"
+    }
+    $i--
+    Start-Sleep -Seconds 1.5
+}
 Pause
