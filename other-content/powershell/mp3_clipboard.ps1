2a14a1bc3189f062002bd8bdb64cb7ad7c50767e
log1x0 <107345625+log1x0@users.noreply.github.com>
2023-02-19 07:48:03 +0100

    Update mp3_clipboard.ps1
    Description added, Write-Output $i out commented ...

--- other-content/powershell/mp3_clipboard.ps1
+++ other-content/powershell/mp3_clipboard.ps1
@@ -1,17 +1,18 @@
 #
-# Loads/converts the text clipboard content from YouTube as MP3...
+# Loads/converts the clipboard text content from YouTube as MP3...
 #
 # Please perform the following steps **once** before:
 # - Download the yt-dlp and ffmpeg binaries
 # -- https://github.com/yt-dlp/yt-dlp
 # -- https://github.com/yt-dlp/FFmpeg-Builds
 # - Make sure to have added an entry to the *path system environment variable* correctly (to the binary folder)
+# -- https://stackoverflow.com/a/46679245
 # - Run "Set-ExecutionPolicy -Scope CurrentUser Unrestricted -Force" in PowerShell
 #
 Add-Type -Assembly PresentationCore
-$i = 1000
+$i = 10000
 while ($i -gt 0) {
-    Write-Output $i
+    # Write-Output $i
     $CLIPBOARD_TEXT = [Windows.Clipboard]::GetText()
     if ($CLIPBOARD_TEXT -match 'https://www\.youtube\.com/watch\?v=([^?&]{6,})') {
         Set-Clipboard -Value "This is a test string"
@@ -21,6 +22,6 @@ while ($i -gt 0) {
         yt-dlp.exe -f bestaudio -x --audio-format mp3 --audio-quality 0 "$VIDEOID"
     }
     $i--
-    Start-Sleep -Seconds 1.5
+    Start-Sleep -Seconds 3
 }
 Pause
